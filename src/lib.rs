ef876aa8a6e0763d267e3603937c12fd83ba2460
XianHuang <148949408+LiDingyiii@users.noreply.github.com>
2024-11-14 17:14:45 +0800

    获取时间以

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use std::io;
-use std::{fs, path::Path};
+use chrono::{DateTime, Utc};
 
 pub mod args;
 pub mod reader;
 pub mod render;
 
+pub fn get_time() -> String {
+    let now: DateTime<Utc> = Utc::now();
+    now.date_naive().to_string()
+}
