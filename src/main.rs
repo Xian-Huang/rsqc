0d8edb33a1999cb26906290cd5622bf457e71625
XianHuang <148949408+LiDingyiii@users.noreply.github.com>
2024-11-14 17:15:29 +0800

    基本结构完成

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
+use std::path::Path;
+
 use clap::Parser;
 use rsqc::{
     args::Args,
+    get_time,
     reader::{data_loader, ReaderConfig},
-    render::{Render, RenderConfig, Source},
+    render::{load_images, Render, RenderConfig, Source},
 };
 use tera::Context;
 
 fn main() {
     let args = Args::parse();
     let config = ReaderConfig::new(args.number_header, args.data); //读取配置
-    let _df = data_loader(&config);
-    let table = vec![vec!["1", "23", "4", "5", "4"]
-        .iter()
-        .map(|&x| x.to_string())
-        .collect()];
+    let df = data_loader(&config);
+    let images = load_images(Path::new(&args.images));
     let product = Source {
-        time: "20240-11-14".to_string(),
-        table,
-        images: vec!["src/images/image.png".to_string()],
+        time: get_time(),
+        table: df.data,
+        images: images,
     };
     let render_config = RenderConfig {
-        template: "D:/ldy/qc_report/template".to_string(),
+        template: args.template,
         context: Context::from_serialize(product).unwrap(),
-        output: "./".to_string(),
+        output: args.output_path,
     };
     let render = Render::new(render_config);
     let _ = render.render();
--- src/render.rs
+++ src/render.rs
@@ -1,6 +1,7 @@
 use std::{
-    fs::{self},
+    fs::{self, read_dir},
     io::Write,
+    path::Path,
 };
 
 use serde::Serialize;
@@ -23,14 +24,22 @@ pub struct Render {
     pub config: RenderConfig,
 }
 
-
 impl Render {
     pub fn new(config: RenderConfig) -> Self {
         Render { config }
     }
-
     pub fn render(self: &Self) {
-        let tera = match Tera::new("D:/ldy/qc_report/template/*.html") {
+        let tempale_path = Path::new(&self.config.template)
+            .canonicalize()
+            .unwrap()
+            .join("*.html");
+
+        let ouput_path = Path::new(&self.config.template)
+            .canonicalize()
+            .unwrap()
+            .join("report.html");
+
+        let tera = match Tera::new(tempale_path.as_path().to_str().unwrap()) {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);
@@ -38,7 +47,21 @@ impl Render {
             }
         };
         let render_string = tera.render("index.html", &self.config.context).unwrap();
-        let mut file = fs::File::create("D:/ldy/qc_report/template/main.html").unwrap();
+        let mut file = fs::File::create(ouput_path).unwrap();
         let _ = file.write(render_string.into_bytes().as_slice());
     }
+}
+
+pub fn load_images(path: &Path) -> Vec<String> {
+    let mut res = vec![];
+    for file in read_dir(path).unwrap() {
+        let file = file.unwrap();
+        let filepath = file.path();
+        if filepath.as_path().to_str().unwrap().ends_with(".png")
+            || filepath.as_path().to_str().unwrap().ends_with(".jpg")
+        {
+            res.push(filepath.to_str().unwrap().to_string());
+        }
+    }
+    res
 }
